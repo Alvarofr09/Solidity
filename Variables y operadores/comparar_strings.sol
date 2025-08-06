00f08b03b0ba01a052ac543723914ce28b84f8bb
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-08-06 20:21:42 +0200

    clase de comparar string con keckak

---
+++ Variables y operadores/comparar_strings.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.0 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+contract compararStrings {
+
+    function Comparar(string memory _i, string memory _j) public pure returns(bool) {
+        bytes32 hash_i = keccak256(abi.encodePacked(_i));
+        bytes32 hash_j = keccak256(abi.encodePacked(_j));
+        
+        if (hash_i == hash_j) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
